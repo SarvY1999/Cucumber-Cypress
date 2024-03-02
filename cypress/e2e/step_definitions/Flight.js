dc1a1b53e422535b2bab642f05e85ba6e0d4f716
SarvY1999 <sarveshy193@gmail.com>
2024-03-02 16:17:49 +0530

    triggering build on push

--- cypress/e2e/step_definitions/Flight.js
+++ cypress/e2e/step_definitions/Flight.js
@@ -1,21 +1,23 @@
-import {Given, When, Then} from "cypress-cucumber-preprocessor/steps"
+import { Given, When, Then } from "cypress-cucumber-preprocessor/steps"
 import Flight from "../Pages/Flight"
 
 const flight = new Flight();
 
-Given('Navigate and go to Flights Section', ()=> {
+Given('Navigate and go to Flights Section', () => {
     cy.visit('https://demo.guru99.com/test/newtours/index.php');
     cy.get('td>a').contains('Flights').click();
 })
 
-Then('Select Flight Type as One Way', ()=> {
+Then('Select Flight Type as One Way', () => {
     flight.selectFlightType();
 });
 
-Then('Select passengers as {string}', (no)=> {
+Then('Select passengers as {string}', (no) => {
     flight.selectPassengers(no);
 });
 
-Then('Select Departing Location {string}', (location)=> {
+Then('Select Departing Location {string}', (location) => {
     flight.selectDeparting(location);
 })
+
+// Adding this line to trigeer build run on push
